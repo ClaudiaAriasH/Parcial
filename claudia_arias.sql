c33a7044d66dd00737e1a318acc84b41469ca220
ClaudiaAriasH <36428554+ClaudiaAriasH@users.noreply.github.com>
2018-04-25 21:46:42 -0500

    sql

---
+++ claudia_arias.sql
@@ -0,0 +1,18 @@
+--------------------------------------------------------
+-- Archivo creado  - miércoles-abril-25-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "EJERCICIOS"."FUNCTION_1" (a IN NUMBER, b IN NUMBER,c IN NUMBER)
+ RETURN VARCHAR2 IS
+  Z VARCHAR2(250);
+ BEGIN
+ IF b>=c THEN
+  Z:= a|| '/'|| a*b|| '/'||'andres';
+ ELSE 
+Z:= a|| '/'||a*b|| '/'||'martinez';
+ END IF;
+ RETURN Z;
+END;
